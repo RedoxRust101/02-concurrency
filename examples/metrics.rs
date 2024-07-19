78d5b84ece01f99a5d3911b963df98ba7644181e
cm <redox-rust@hotmail.com>
2024-07-19 10:54:50 +0800

    fix: do not use unwrap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -14,11 +14,11 @@ fn main() -> Result<()> {
     println!("start {} workers", N);
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        task_worker(idx, metrics.clone())?;
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
 
     loop {
@@ -27,25 +27,31 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
     thread::spawn(move || {
         loop {
             // do long term stuff
             let mut rng = rand::thread_rng();
             thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-            metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
         }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
+fn request_worker(metrics: Metrics) -> Result<()> {
     thread::spawn(move || {
         loop {
             // process request
             let mut rng = rand::thread_rng();
             thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
             let page = rng.gen_range(1..5);
-            metrics.inc(format!("req.page.{}", page)).unwrap();
+            metrics.inc(format!("req.page.{}", page))?;
         }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
